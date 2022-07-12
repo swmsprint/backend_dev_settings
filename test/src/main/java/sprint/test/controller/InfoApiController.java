f0107aedd0791afb9841677575adf9f7d697f8c0
yewon <yewon7036@naver.com>
2022-07-12 16:48:42 +0900

    mariadb testcode

--- test/src/main/java/sprint/test/controller/InfoApiController.java
+++ test/src/main/java/sprint/test/controller/InfoApiController.java
@@ -1,16 +1,18 @@
 package sprint.test.controller;
 
-import org.springframework.stereotype.Controller;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import sprint.test.domain.Info;
-import sprint.test.domain.InfoForm;
 
-@Controller
-public class HelloController {
+
+@Slf4j
+@RestController
+public class InfoApiController {
     /**
      *
      * welcome반환
-     */
+//     */
     @GetMapping("/http/get")
     @ResponseBody
     public String apiWelcome(){
